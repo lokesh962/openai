2c55b5be769eb76e3d550490aea0187159ee40d0
lokesh962 <85404656+lokesh962@users.noreply.github.com>
2023-02-12 20:58:12 +0530

    my repo

--- src/App.js
+++ src/App.js
@@ -1,24 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import Navbar from "./components/Navbar";
+import { Configuration, OpenAIApi } from "openai";
+
 
 function App() {
+  const apiKey='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'
+  const configuration = new Configuration({
+    organization: "org-dtEIdqc1TzWQY8UYA6a3Xep6",
+    apiKey: apiKey,
+});
+
+
+  const [question,setQuestion]=useState("")
+  const chats=document.getElementById('send')
+  
+  const onClick=async(e)=>{
+    const questionElement=document.getElementById('question').value=""
+    
+    chats.innerHTML=chats.innerHTML+`<div class="send chat text-white">
+    <p>${question}</p>
+    </div>`
+    
+
+
+const openai = new OpenAIApi(configuration);
+const response = await openai.createCompletion({
+  model: "text-davinci-003",
+  prompt: question,
+  max_tokens: 4000,
+  temperature: 0,
+});
+
+const a=response.data.choices[0].text.split('\n').filter((value)=>value).map(value=>value.trim())
+    // console.log(a);
+    const b=a.forEach(element=>{return `<p>${element}</p>`})
+  console.log(b);
+  
+
+    chats.innerHTML=chats.innerHTML+`<div class="recieve chat text-white">
+    <p>${a}</p>
+  </div>`
+
+    
+  }
+
+  const OnChange=(e)=>{
+    setQuestion(e.target.name=e.target.value)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <Navbar/>
+    <div>
+      <h1 className="text-white text-center">Welcome To LKH AI </h1>
+      
+      </div>
+    <div className="container">
+      <div className="chats" id="send">
+
+        <div className="recieve chat text-white">
+          <p>Hi Human ,How Can i help you?</p>
+        </div>
+      </div>
+      <div className="chat-input">
+        <input type="text" id ="question" name="question" className="form-control col" onChange={OnChange} placeholder="Type Somethings"/>
+      <button className="btn btn-success" id="submitBtn" onClick={onClick}>Send</button>
+      </div>
     </div>
+    </>
   );
 }
 
